8c4c894c97fb9cc6b9003a3fa1d29f221a3065eb
William Liu <williamliu414@gmail.com>
2023-10-06 16:57:33 +0100

    Finished challenge 1

--- src/model/train.py
+++ src/model/train.py
@@ -3,9 +3,9 @@
 import argparse
 import glob
 import os
-
+import mlflow
 import pandas as pd
-
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 
@@ -13,7 +13,7 @@ from sklearn.linear_model import LogisticRegression
 def main(args):
     # TO DO: enable autologging
 
-
+    mlflow.autolog()
     # read data
     df = get_csvs_df(args.training_data)
 
@@ -32,8 +32,13 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
-
 # TO DO: add function to split data
+def split_data(df):
+    from sklearn.model_selection import train_test_split
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    print(f"\n{np.unique(y, return_counts=True)}\n")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
